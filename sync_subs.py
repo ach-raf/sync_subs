56a4719022977d5104e0d44298838d70231a70e6
ach-raf <35491497+ach-raf@users.noreply.github.com>
2021-02-13 23:10:56 +0100

    Add files via upload

--- sync_subs.py
+++ sync_subs.py
@@ -1,13 +1,14 @@
 import subprocess
 import sys
 
-_paths = sys.argv[1:]
+_paths = sys.argv[1:]  # list of arguments sent to the script
 for _path in _paths:
+    # using subsync library to do the magic
     _command = ['ffs',
-                _path,
-                '-i', f'{_path[:-4]}.srt',
-                '-o', f'{_path[:-4]}.srt',
-                '--encoding', 'utf-8']
+                _path,  # path to the video
+                '-i', f'{_path[:-4]}.srt',  # the subtitle for input, using the same name as the film + .srt
+                '-o', f'{_path[:-4]}.srt',  # the output replaces the original subtitle
+                '--encoding', 'utf-8']  # encoding
     subprocess.run(_command)
 
 
