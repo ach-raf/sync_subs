9b2abf12eafc74c9a67efc124fcf4bcc89edddd9
achraf.zouchou <achraf.zouchou@gmail.com>
2022-03-29 17:26:01 +0000

    fixed file not found error

--- sync_subs.py
+++ sync_subs.py
@@ -1,14 +1,45 @@
 import subprocess
 import sys
+import os
+import shutil
 
-_paths = sys.argv[1:]  # list of arguments sent to the script
-for _path in _paths:
+
+CURRENT_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def sync_subs_srt(_reference_srt, _unsync_srt, _output):
+    _command = [
+        shutil.which("ffs"),
+        {_reference_srt},
+        "-i",
+        {_unsync_srt},
+        "-o",
+        {_output},
+    ]
+    subprocess.call(_command)
+
+
+def sync_subs_audio(_paths):
     # using subsync library to do the magic
-    _command = ['ffs',
-                _path,  # path to the video
-                '-i', f'{_path[:-4]}.srt',  # the subtitle for input, using the same name as the film + .srt
-                '-o', f'{_path[:-4]}.srt',  # the output replaces the original subtitle
-                '--encoding', 'utf-8']  # encoding
-    subprocess.run(_command)
+    _command = [
+        shutil.which("ffs"),
+        rf"{_paths}",  # path to the video
+        "-i",
+        rf"{_paths[:-4]}.srt",  # the subtitle for input, using the same name as the film + .srt
+        "-o",
+        rf"{_paths[:-4]}.srt",  # the output replaces the original subtitle
+        "--encoding",
+        "utf-8",
+    ]  # encoding
+    subprocess.call(_command)
 
 
+def main():
+    _paths = sys.argv[1:]  # list of arguments sent to the script
+    for _path in _paths:
+        clean_path = os.path.normpath(_path)
+        sync_subs_audio(clean_path)
+
+
+if __name__ == "__main__":
+    main()
